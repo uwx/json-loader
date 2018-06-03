79601b8edade800f39861277189b15d7ad90577d
uwx <chris331hansen@outlook.com>
2018-06-03 14:38:53 -0300

    fix const error

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const yaml = require('js-yaml');
 module.exports = source => {
   if (this.cacheable) this.cacheable();
 
-  const value = JSON.stringify(yaml.safeLoad(source));
+  let value = JSON.stringify(yaml.safeLoad(source));
 
   value = value
     .replace(/\u2028/g, '\\u2028')

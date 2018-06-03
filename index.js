8a4409072d96baaa15fc1e4380ebc1619decf9ed
uwx <chris331hansen@outlook.com>
2018-06-03 14:45:11 -0300

    fix proper now i think?

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ const yaml = require('js-yaml');
 module.exports = source => {
   if (this.cacheable) this.cacheable();
 
-  let value = JSON.stringify(yaml.safeLoad(source));
-
-  value = value
+  const value = JSON.stringify(yaml.safeLoad(source))
     .replace(/\u2028/g, '\\u2028')
     .replace(/\u2029/g, '\\u2029');
 
